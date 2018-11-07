209c0ce91387d1c7a9a50e2638e2ca8ea9c3d21a
Jo Minji <mingi2687@gamil.com>
2018-11-07 18:24:37 +0900

    리스트 부름, 로그인 창 띄우기 버그 있음

--- src/index.js
+++ src/index.js
@@ -16,8 +16,10 @@ api.interceptors.request.use(function (config) {
 });
 
 const templates = {
-
-}
+  loginForm: document.querySelector("#login-form").content,
+  MainEl: document.querySelector("#main").content,
+  MainProductDetail: document.querySelector("#main-product-detail").content
+};
 
 const rootEl = document.querySelector('.root')
 
@@ -28,3 +30,108 @@ const rootEl = document.querySelector('.root')
 // 4. 내용 채우기
 // 5. 이벤트 리스너 등록하기
 // 6. 템플릿을 문서에 삽입
+
+
+
+
+
+//메인시작
+
+async function drawMain() {
+  // 1. 템플릿 복사
+  //메인 템플릿 복사
+  const frag = document.importNode(templates.MainEl, true)
+
+  // 2. 요소 선택
+  //로그인 버튼 선택
+  const loginButtonEl = frag.querySelector('.login')
+
+
+  // 3. 필요한 데이터 불러오기
+
+  // 4. 내용 채우기
+  //버튼 클릭할 때 로그인폼 띄우기
+  loginButtonEl.addEventListener("click", e => {
+    console.log("로그인버튼");
+    drawLoginForm();
+  });
+
+
+  MainProductDetail();
+
+  rootEl.textContent = ''
+  rootEl.appendChild(frag)
+
+
+
+  }
+
+  //메인끝
+
+
+
+
+async function drawLoginForm() {
+  // 1. 템플릿 복사
+  const frag = document.importNode(templates.loginForm, true);
+
+  // 2. 요소 선택
+  const formEl = frag.querySelector(".login-form");
+
+  // 3. 필요한 데이터 불러오기 - 필요없음
+  // 4. 내용 채우기 - 필요없음
+  // 5. 이벤트 리스너 등록하기
+  formEl.addEventListener("submit", async e => {
+    e.preventDefault();
+    const username = e.target.elements.username.value;
+    const password = e.target.elements.password.value;
+    const res = await api.post("/users/login", {
+      username,
+      password
+    });
+
+    localStorage.setItem("token", res.data.token);
+    drawMain();
+  });
+
+  // 6. 템플릿을 문서에 삽입
+  rootEl.textContent = ""; //??
+  rootEl.appendChild(frag);
+}
+
+//메인 리스트 세부사항
+
+
+async function MainProductDetail() {
+  // 1. 템플릿 복사
+  const frag = document.importNode(templates.MainEl, true);
+
+  // 2. 요소 선택
+  const mainProductListEl = frag.querySelector(".product-list");
+
+   // 3. 필요한 데이터 불러오기
+  //상품 데이터 불러오기
+  const { data: productList } = await api.get("/products");
+  console.log(productList);
+  // 4. 내용 채우기
+  //상품 리스트 가져오기
+  for (const productItem of productList ) {
+    const frag = document.importNode(templates.MainProductDetail, true);
+
+    const titleEl = frag.querySelector(".title");
+    const priceEl = frag.querySelector(".price");
+
+    titleEl.textContent = productItem.title;
+    // priceEl.textContent = productItem.price;
+
+    mainProductListEl.appendChild(frag);
+
+  }
+
+  rootEl.textContent = "";
+  rootEl.appendChild(frag);
+
+}
+
+drawMain();
+
