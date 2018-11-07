62db41dcb52cf5ac19a90960bea08ed7562b51a1
Jo Minji <mingi2687@gamil.com>
2018-11-07 22:01:50 +0900

    사진추가

--- src/index.js
+++ src/index.js
@@ -37,24 +37,30 @@ const rootEl = document.querySelector('.root')
 
 //메인시작
 
-async function drawMain() {
+async function drawMain(){
   // 1. 템플릿 복사
   //메인 템플릿 복사
+
   const frag = document.importNode(templates.MainEl, true)
 
   // 2. 요소 선택
   //로그인 버튼 선택
-  const loginButtonEl = frag.querySelector('.login')
-
+  const loginButtonEl = frag.querySelector(".login");
+  console.log(loginButtonEl);
 
   // 3. 필요한 데이터 불러오기
 
   // 4. 내용 채우기
+  console.log('aa');
+  loginButtonEl.addEventListener('click', (e) => {
+    console.log('click');
+  })
   //버튼 클릭할 때 로그인폼 띄우기
-  loginButtonEl.addEventListener("click", e => {
-    console.log("로그인버튼");
-    drawLoginForm();
-  });
+  // loginButtonEl.addEventListener("click", e => {
+  //   e.preventDefault();
+  //   console.log("클릭");
+  //   drawLoginForm();
+  // });
 
 
   MainProductDetail();
@@ -94,13 +100,12 @@ async function drawLoginForm() {
     drawMain();
   });
 
-  // 6. 템플릿을 문서에 삽입
-  rootEl.textContent = ""; //??
+
+  rootEl.textContent = "";
   rootEl.appendChild(frag);
 }
 
 //메인 리스트 세부사항
-
 
 async function MainProductDetail() {
   // 1. 템플릿 복사
@@ -109,20 +114,34 @@ async function MainProductDetail() {
   // 2. 요소 선택
   const mainProductListEl = frag.querySelector(".product-list");
 
+
    // 3. 필요한 데이터 불러오기
   //상품 데이터 불러오기
-  const { data: productList } = await api.get("/products");
-  console.log(productList);
+
+
+  const { data: productList } = await api.get("/products", {
+    params: {
+      _embed: "options",
+    }
+  });
+
   // 4. 내용 채우기
   //상품 리스트 가져오기
   for (const productItem of productList ) {
+    //li리스트 템플릿 복사
     const frag = document.importNode(templates.MainProductDetail, true);
 
+    //li 리스트 안에 요소 선택
+    const mainProductImgEl = frag.querySelector(".product_img>img");
     const titleEl = frag.querySelector(".title");
     const priceEl = frag.querySelector(".price");
 
     titleEl.textContent = productItem.title;
-    // priceEl.textContent = productItem.price;
+    priceEl.textContent = productItem.options[0].price;
+    // console.log(productItem);
+
+    mainProductImgEl.setAttribute("src",productItem.mainImgUrl);
+
 
     mainProductListEl.appendChild(frag);
 
